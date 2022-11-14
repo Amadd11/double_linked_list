ff807ae260f8615bf434d8861be18d6c76d1711c
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-11-14 16:57:37 +0700

    Add constructor

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -8,7 +8,38 @@ namespace double_linked_list
 {
     class Node
     {
+        /* Node class represents the node of doubly linked list.
+         * it consits of the information part and links to
+         * its succeeding and preceeding
+         * in terms of next and previous */
+        public int noMhs;
+        public string name;
+        //point to succeding node
+        public Node next;
+        //point to the precceeding node
+        public Node prev;
+    }
+     class DoubleLinkedList
+    {
+        Node START;
 
+        //constructor
+        public DoubleLinkedList()
+        {
+            START = null;
+        }
+        public void addNode()
+        {
+            int nim;
+            string nm;
+            Console.Write("\nEnter the roll number of the student: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student:");
+            nm = Console.ReadLine();
+            Node newNode = new Node();
+            newNode.noMhs = nim;
+            newNode.name = nm;
+        }
     }
      class Program
     {
