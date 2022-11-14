67fb514aaba40c053c4a42d74c3047f32415135a
Muhammad Syafei <muhammad.syafei.ft21@mail.umy.ac.id>
2022-11-14 17:18:56 +0700

    Add Method Search and function for

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -55,6 +55,40 @@ namespace double_linked_list
                 START = newNode;
                 return;
             }
+            /*if the node is to be inserted at between two node*/
+            Node previous, current;
+            for (current = previous = START;
+                current != null && nim >= current.noMhs;
+                previous = current, current = current.next)
+            {
+                if(nim == current.noMhs)
+                {
+                    Console.WriteLine("\nDuplictae roll numbes not allowed");
+                    return;
+                }
+            }
+            /*on the execution of the above for loop, prev and 
+             * * current will point to those nodes
+             * * between wich the new node is to be inserted.*/
+            newNode.next = current;
+            newNode.prev = previous;
+
+            //if the node is to be insarted at the end of the list
+            if(current == null)
+            {
+                newNode.next = null;
+                previous.next = newNode;
+                return;
+            }
+            current.prev = newNode;
+            previous.next = newNode;
+        }
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            for (previous = current = START; current != null &&
+                rollNo != current.noMhs; previous = current,
+                current = current.next) { }
+            return (current!= null);
         }
     }
      class Program
